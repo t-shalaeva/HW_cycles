80ce094acc4ff7e948a128dc047fc5810cf5981c
Tatiana Shalaeva <alice.contist@gmail.com>
2024-10-12 17:40:51 +0200

    add HW

---
+++ src/test/java/ru/netology/services/VacationCalculatorTest.java
@@ -0,0 +1,20 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class VacationCalculatorTest {
+    @ParameterizedTest
+    @CsvSource({
+            "10000, 3000, 20000, 3",
+            "100000, 60000, 150000, 2"
+    })
+    public void shouldCalcVacations (int income, int expenses, int threshold, int expected){
+        VacationCalculator vacation = new VacationCalculator();
+
+        int actual = vacation.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected,actual);
+    }
+}
